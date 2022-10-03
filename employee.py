14f87a463a6634d691e2460d5b08a19a049263db
Oliver Singer <owsinger@outlook.com>
2022-10-03 09:56:10 +0100

    Fixed several issues in __str__ method

--- employee.py
+++ employee.py
@@ -13,26 +13,30 @@ class Employee:
     def get_pay(self):
         if self.is_monthly == True:
             if self.commission_number > 0:
-                return(str((self.salary)+(self.commission_number*self.contract_worth)))
+                return ((self.salary)+(self.commission_number*self.contract_worth))
             else:
-                return(str(self.salary))
+                return (self.salary)
         else:
             if self.commission_number > 0:
-                return(str((self.salary*self.hours)+(self.commission_number*self.contract_worth)))
+                return ((self.salary*self.hours)+(self.commission_number*self.contract_worth))
             else:
-                return(str(self.salary*self.hours))
+                return (self.salary*self.hours)
 
     def __str__(self):
         if self.is_monthly == True:
-            if self.commission_number > 0:
-                exit
+            if self.commission_number > 1:
+                print(self.name + " works on a monthly salary of " + str(self.salary) + " and receives a commission for " + str(self.commission_number) + " contract(s) at " + str(self.contract_worth) + "/contract. Their total pay is " + str(self.get_pay()) + ".")
+            elif self.commission_number == 1:
+                print(self.name + " works on a monthly salary of " + str(self.salary) + " and receives a bonus commission of " + str(self.contract_worth) + ". Their total pay is " + str(self.get_pay()) + ".")
             else:
-                print(self.name + " works on a monthly salary of " + str((self.get_pay())))
+                print(self.name + " works on a monthly salary of " + str(self.salary) + ". Their total pay is "+ str(self.get_pay()) + ".")
         else:
-            if self.commission_number > 0:
-                exit
+            if self.commission_number > 1:
+                print(self.name + " works on a contract of " + str(self.hours) + " hours at " + str(self.salary) + "/hour and receices a commission for " + str(self.commission_number) + " contract(s) at " + str(self.contract_worth) + "/contract. Their total pay is "+str(self.get_pay()) + ".")
+            elif self.commission_number == 1:
+                print(self.name + " works on a contract of " + str(self.hours) + " hours at " + str(self.salary) + "/hour and receices a bonus commission of " + str(self.contract_worth) + ". Their total pay is "+str(self.get_pay()) + ".")
             else:
-                print(self.name + "works on a contract of " + str(self.hours) + " at " + str(self.salary) + "/hour. Their total pay is " + str(self.get_pay()))
+                print(self.name + " works on a contract of " + str(self.hours) + " hours at " + str(self.salary) + "/hour. Their total pay is " + str(self.get_pay()) + ".")
 
 
 # Billie works on a monthly salary of 4000.  Their total pay is 4000.
@@ -52,11 +56,3 @@ robbie = Employee('Robbie', True, 0, 2000, 1, 1500)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Employee('Ariel', False, 120, 30, 1, 600)
-
-billie.get_pay()
-charlie.get_pay()
-renee.get_pay()
-jan.get_pay()
-robbie.get_pay()
-ariel.get_pay()
-billie.__str__()
